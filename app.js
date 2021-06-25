9537296776897447b5f317a46a3dee5159b6c841
Dele O <717638+deletosh@users.noreply.github.com>
2021-06-25 09:43:56 -0400

    chore: refactor divs into fns

--- app.js
+++ app.js
@@ -1,50 +1,30 @@
 // pseudo-code
-// goal: clicks a button AMD generates a new div with new color
+// goal: clicks a button AND generates a new div with new color
 
 // 1. get the the button
-let btnGen = document.querySelector('#btn-generate')
-let btnGenTwo = document.querySelector('#btn-generate-two')
+const btnGen = document.querySelector('#btn-generate');
+const btnGenTwo = document.querySelector('#btn-generate-two');
 
 // 2. be able to click the button
 // 3. After button click, generate div
-btnGen.addEventListener('click', function(){
-    console.log('I click a button....');
-
-    let div = document.createElement('div');
-    div.innerHTML = '<h3 class="text-9xl bg-blue-400 p-3">My Big, Big H3</h3>';
-
-    document.body.appendChild(div);
-})
+btnGen.addEventListener('click', function () {
+    createNewDiv('bg-blue-400', 'My Big, Big H3');
+});
 
 btnGenTwo.addEventListener('click', () => {
+    createNewDiv('bg-gray-300', 'My New New');
+});
 
-    let divTwo = document.createElement('div');
-    divTwo.innerHTML = '<h3 class="text-9xl bg-green-400 p-3">My Second H3</h3>';
+// [D]ry [R]epeat [Y]ourself
+function createNewDiv(tailwindBgColor, content) {
+    const div = document.createElement('div');
+    div.innerHTML = `<h3 class="text-9xl ${tailwindBgColor} p-3">${content}</h3>`; // string interpolation
 
-    document.body.appendChild(divTwo);
+    document.body.appendChild(div);
 
-})
+}
 
-// console.log(btnGen);
-//
-//
-// // build dynamic H2
-// let h2 = document.createElement('h2'); // fn
-// h2.innerText = 'Some H2 Content'; // properties
-//
-// // build div
+// console.log(btnGen); // my string
 
 
 
-
-// div.innerText = 'My String';
-
-// CMD + ,
-// Alt + Ctl + S
-
-
-// my appends
-document.body.appendChild(h2);
-
-//
-// console.log('new H2: ', h2);

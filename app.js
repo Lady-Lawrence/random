ec1d0d19976c8c45fec1515ce86fe3653d3c3451
Dele O <717638+deletosh@users.noreply.github.com>
2021-06-28 11:05:17 -0400

    rfc: refactor `btnGen` eventListener

--- app.js
+++ app.js
@@ -3,21 +3,41 @@
 
 // 1. get the the button
 const btnGen = document.querySelector('#btn-generate');
-const btnGenTwo = document.querySelector('#btn-generate-two');
 
 // 2. be able to click the button
 // 3. After button click, generate div
 btnGen.addEventListener('click', function () {
 
+    // collectInput()
+    // checkInput()
+    // createDiv()
+
+    collectInput(); //string
+
+    //@TODO: Refactor this block to simplify this EventListner
     const userTailwindColor = window.prompt('What TW Color?'); //string
     const userContent = window.prompt('What Text?'); // string
 
+    // 1. check that the input is valid
+    // 2. if it's not valid, give feedback
+
+    checkInput(); //true
+    // 3. if not OK stop
+    // return ;
+
+    // 4. if OK
     createNewDiv(userTailwindColor, userContent);
+
 });
 
-btnGenTwo.addEventListener('click', () => {
-    createNewDiv('bg-gray-300', 'My New New');
-});
+function collectInput(){
+    //@TODO: collect user input
+}
+
+function checkInput() {
+    // @TODO: check if input is valid
+    return true;
+}
 
 // [D]ry [R]epeat [Y]ourself
 function createNewDiv(tailwindBgColor, content) {
@@ -29,11 +49,6 @@ function createNewDiv(tailwindBgColor, content) {
     window.alert('Your div was created');
 }
 
-
-// checks if you enter a name
-function hasName() {
-    return true;
-}
 
 
 
